5551765071534e18ef19d6480c5d0e69f998724e
khoirxz <khoirrurrohman@gmail.com>
2025-10-09 07:34:23 +0700

    random string

---
+++ stringrandom.js
@@ -0,0 +1,14 @@
+// Generate a random string of given length
+function generateRandomString(length) {
+  const chars =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  let result = "";
+  for (let i = 0; i < length; i++) {
+    result += chars.charAt(Math.floor(Math.random() * chars.length));
+  }
+  return result;
+}
+
+// Example usage:
+const length = 12; // Change this to desired length
+console.log("Random string:", generateRandomString(length));
